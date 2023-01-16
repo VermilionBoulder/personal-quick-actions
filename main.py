fd841d091ef86e34a7e05299898c9d3994fb8ddb
ksurowka <kacper.surowka@nokia.com>
2023-01-16 23:19:13 +0100

    Added __init__.py to modules directory. Improved imports.

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import time
 import tkinter as tk
 import tkinter.font as tk_font
-import modules
+
+from modules import *
 
 
 class App:
     def __init__(self, root):
         self.root = root
-        self.root.title("undefined")
+        self.root.title("Personal Quick Actions")
         self.root.resizable(width=False, height=False)
         self.buttons = []
         self.labels = []
         self.root.bind('<Escape>', lambda e: self.root.destroy())
 
-        # config shared by all buttons & labels
-        self.common_config = {
+        self.button_and_label_common_config = {
             "button": {
                 "bg": "#e9e9ed",
                 "font": tk_font.Font(family="Segoe UI", size=12),
@@ -33,30 +33,30 @@ class App:
             {
                 "text": "v",
                 "label": "Format VLANs for VLAN ticket",
-                "command": modules.vlan_formatter.format_vlan_text,
+                "command": vlan_formatter.format_vlan_text,
                 "exit": True,
             },
             {
                 "text": "y",
                 "label": "Download YouTube video from copied link",
-                "command": modules.youtube_downloader.download_video,
+                "command": youtube_downloader.download_video,
                 "exit": True,
             },
             {
                 "text": "a",
                 "label": "Take text from clipboard and make it aLtCaPs",
-                "command": modules.alt_caps.alt_caps,
+                "command": alt_caps.alt_caps,
                 "exit": True,
             },
             {
                 "text": "o",
                 "label": "Open OneLab page for copied Hardware IDs",
-                "command": modules.onelab_hardware_details.open_onelab_pages,
+                "command": onelab_hardware_details.open_onelab_pages,
                 "exit": True,
             },
         ]
 
-        width = 300
+        width = 380
         height = 20 + 40 * len(self.specific_configs)
         screenwidth = self.root.winfo_screenwidth()
         screenheight = self.root.winfo_screenheight()
@@ -67,13 +67,17 @@ class App:
     def define_options(self):
         # For every button defined in self.specific_configs above, add a button and label pair
         for idx, config in enumerate(self.specific_configs):
-            new_button = tk.Button(self.root, cnf=self.common_config.get("button"), text=config.get("text"),
+            new_button = tk.Button(self.root,
+                                   cnf=self.button_and_label_common_config.get("button"),
+                                   text=config.get("text"),
                                    command=config.get("command"))
             new_button.place(x=10, y=10 + 40 * idx, width=30, height=30)
             self.buttons.append(new_button)
 
-            new_label = tk.Label(self.root, cnf=self.common_config.get("label"), text=config.get("label"))
-            new_label.place(x=45, y=10 + 40 * idx, width=200, height=30)
+            new_label = tk.Label(self.root,
+                                 cnf=self.button_and_label_common_config.get("label"),
+                                 text=config.get("label"))
+            new_label.place(x=45, y=10 + 40 * idx, width=350, height=30)
             self.labels.append(new_label)
 
             self.add_binding(config)
---
+++ modules/__init__.py
@@ -0,0 +1,4 @@
+from . import alt_caps
+from . import onelab_hardware_details
+from . import vlan_formatter
+from . import youtube_downloader
