c459bc5e93eee4f90334e0363214ad32b0418dbc
ksurowka <kacper.surowka@nokia.com>
2023-01-16 18:37:42 +0100

    Streamlined adding bindings to root

--- main.py
+++ main.py
@@ -8,73 +8,88 @@ class App:
         # for loop
         self.root = root
         self.root.title("undefined")
+        self.root.resizable(width=False, height=False)
+        self.buttons = []
+        self.labels = []
+
+        self.common_config = {
+            "button": {
+                "bg": "#e9e9ed",
+                "font": tkFont.Font(family="Segoe UI", size=12),
+                "fg": "#000000",
+                "justify": "center",
+            },
+            "label": {
+                "font": tkFont.Font(family="Segoe UI", size=12),
+                "fg": "#333333",
+                "justify": "left",
+            },
+        }
+
+        self.specific_configs = [
+            {
+                "text": "1",
+                "label": "say shit",
+                "command": self.say_shit,
+            },
+            {
+                "text": "a",
+                "label": "button command",
+                "command": self.button_command,
+            },
+            {
+                "text": "Z",
+                "label": "say boobs",
+                "command": self.say_boobs,
+            },
+        ]
+
         width = 300
-        height = 300
+        height = 20 + 40 * len(self.specific_configs)
         screenwidth = self.root.winfo_screenwidth()
         screenheight = self.root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         self.root.geometry(alignstr)
-        self.root.resizable(width=False, height=False)
-        self.button = []
+        self.define_options()
 
-    def buttons(self):
-        common_config = {
-            "bg": "#e9e9ed",
-            "font": tkFont.Font(family="Segoe UI", size=12),
-            "fg": "#000000",
-            "justify": "center",
-        }
+    def define_options(self):
+        # For every button defined in list above, add a button and label pair
+        for idx, config in enumerate(self.specific_configs):
+            new_button = tk.Button(self.root, cnf=self.common_config.get("button"), text=config.get("text"),
+                                   command=config.get("command"))
+            new_button.place(x=10, y=10 + 40 * idx, width=30, height=30)
+            cmd = config.get("command")
+            text = config.get("text")
+            # self.root.bind(text, lambda event: cmd.)
+            self.buttons.append(new_button)
 
-        specific_configs = [
-            {
-                "text": "1",
-                "command": self.say_shit(),
-            },
-            {
-                "text": "2",
-                "command": self.button_command()
-            }
-        ]
+            new_label = tk.Label(self.root, cnf=self.common_config.get("label"), text=config.get("label"))
+            new_label.place(x=45, y=10 + 40 * idx, width=200, height=30)
+            self.labels.append(new_label)
 
-        list_of_button_configs = [common_config + specific_config for specific_config in specific_configs]
-        for idx, btn_cnf in enumerate(list_of_button_configs, start=1):
-            self.button.append(tk.Button(cnf=btn_cnf))
-            x, y, width, height = btn_cnf.get("position")
-            self.button[-1].place(x=x, y=y, width=width, height=height)
-            self.root.bind(btn_cnf.get("text"), lambda event: btn_cnf.get("command"))
+        for binding in self.specific_configs:
+            self.add_binding(binding)
 
-    def define_button(self, btn_cnf):
-        btn_cnf = common_config + specific_config
-        position_dict
+    def add_binding(self, binding):
+        # self.root.bind(f"<KeyPress-{binding.get('text')}>", binding.get("command"))
+        self.root.bind(
+            sequence=f"<KeyPress-{binding.get('text')}>",
+            func=(lambda event: f"{binding.get('command')()}"),
+            add=True
+        )
 
-        self.button = tk.Button(self.root)
-        self.button["bg"] = "#e9e9ed"
-        ft = tkFont.Font(family='Segoe UI', size=12)
-        self.button["font"] = ft
-        self.button["fg"] = "#000000"
-        self.button["justify"] = "center"
-        self.button["text"] = "Button"
-        self.button.place(x=10, y=40, width=70, height=25)
-        self.button["command"] = self.button_command
-        self.bindings()
-
-        label = tk.Label(self.root)
-        ft = tkFont.Font(family='Segoe UI', size=12)
-        label["font"] = ft
-        label["fg"] = "#333333"
-        label["justify"] = "center"
-        label["text"] = "label"
-        label.place(x=10, y=10, width=70, height=25)
-
-    def bindings(self):
-        self.root.bind("1", lambda event: self.button_command())
-
+    # def bindings(self):
+    #     self.root.bind("1", lambda event: self.button_command())
 
     def button_command(self):
         print("command")
 
     def say_shit(self):
         print("shit")
+
+    def say_boobs(self):
+        print("boobs")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
